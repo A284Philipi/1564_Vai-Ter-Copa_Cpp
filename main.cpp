5af7efc5d7656e5811009ef138b5aadbc9ec1791
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-21 21:12:21 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int reclamacoes;
+    while (cin >> reclamacoes){
+        if (reclamacoes == 0){
+            cout << "vai ter copa!" <<endl;
+        }else{
+            cout << "vai ter duas!" <<endl;
+        }
+    }
+    return 0;
+}
